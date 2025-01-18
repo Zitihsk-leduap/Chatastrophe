3f23cae953494ce56656c9d473f925ab3bbe0cdc
kshitizHancy <kshitizpaudel5@gmail.com>
2025-01-18 09:42:28 +0545

    Now to Private Chat

--- Chat/rt_chat/consumers.py
+++ Chat/rt_chat/consumers.py
@@ -74,8 +74,8 @@ class ChatroomConsumer(WebsocketConsumer):
     self.send(text_data=html)
 
 
-  def update_online_count(self):
-    online_count = self.chatroom.users_online.count()
+  def update_online_count(self):  
+    online_count = self.chatroom.users_online.count()-1
 
     event ={
       'type':'online_count_handler',
